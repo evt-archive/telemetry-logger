7d34778e0abdb2b32739aca85da60e6b3327bea4
Scott Bellware <sbellware@gmail.com>
2016-05-27 21:03:24 -0600

    Using test bench as a runner (but legacy tests are not compliant with test bench standards)

--- test.rb
+++ test.rb
@@ -1,7 +1,1 @@
-require_relative 'init'
-
-require_relative './test/backward_compatibility'
-require_relative './test/write_log_messages'
-require_relative './test/write_to_null_logger'
-require_relative './test/ad_hoc_debug_message'
-require_relative './test/ad_hoc_logger'
+require_relative 'test/bench'
