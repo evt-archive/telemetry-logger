6d1a11b58712b96e16cf94fb532dd6f9eb119b83
Scott Bellware <sbellware@gmail.com>
2016-05-27 21:18:52 -0600

    Vestigial file removed

--- test/backward_compatibility.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../test_init'
-
-logger = Telemetry::Logger.get self
-logger.info 'Telemetry::Logger.get'
-
-logger = Telemetry::Logger.register self
-logger.info 'Telemetry::Logger.register'
